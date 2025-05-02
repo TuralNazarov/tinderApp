d7725154b251ee4de767f956d49b76a927d58d0b
4DMiR4L <153876304+4DMiR4L@users.noreply.github.com>
2025-05-02 12:42:03 +0400

    test commit

---
+++ src/main/java/project/tinderapp/TinderAppApplication.java
@@ -0,0 +1,13 @@
+package project.tinderapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinderAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinderAppApplication.class, args);
+    }
+
+}
